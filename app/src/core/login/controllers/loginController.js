6e408071900ebe1b5512d336c7deece1742ef346
Poonam Ranawat <p.ranawat@easternenterprise.com>
2017-05-23 13:31:52 +0530

    refresh token changes

--- app/src/core/login/controllers/loginController.js
+++ app/src/core/login/controllers/loginController.js
@@ -1,9 +1,61 @@
 angular.module('login', [])
-    .controller("loginController" , ['$scope' ,'loginService','$auth', function ($scope, loginService, $auth) {
+    .controller("loginController" , ['$scope' ,'loginService','$auth','$timeout', function ($scope, loginService, $auth,$timeout) {
 
-        $scope.login = function (username, password) {
-            $scope.access = loginService.login(username, password).then(function(response){
-                $auth.setToken(response.data["access-token"]);
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        var hh = today.getHours();
+        var min = today.getMinutes();
+        var secs = today.getSeconds();
+
+        if(dd<10) {
+            dd='0'+dd;
+        }
+
+        if(mm<10) {
+            mm='0'+mm;
+        }
+
+        today = mm+'/'+dd+'/'+yyyy + " " + hh + ":" + min + ":" + secs;
+
+
+        $scope.login = function (user, pwd) {
+            var password = 'password';
+            var client_secret = 'l7irzNflVFLaYnJ0mnlC21jSoRfavXPKmHCIFBU2';
+            var data = {
+                grant_type: password,
+                client_id:2,
+                client_secret:client_secret,
+                username: user,
+                password: pwd
+            }
+
+            $scope.access = loginService.login(data).then(function(response){
+                if(response.data.status_code == 200 && response.data.success){
+                    localStorage.setItem("expiry_time",response.data.data.expires_in);
+                    localStorage.setItem('refresh_token',response.data.data.refresh_token);
+                    var expiry = JSON.parse(localStorage.getItem("expiry_time"));
+                    $auth.removeToken();
+                    $auth.setToken(response.data.data.access_token);
+                    setInterval(function(){refresh()},60000);
+                }
             });
+
+            function refresh() {
+                var refresh_token = 'refresh_token';
+                data ['grant_type'] =  refresh_token;
+                data ['refresh_token'] = localStorage.getItem("refresh_token");
+                $scope.access = loginService.refresh(data).then(function (response) {
+                    if(response.data.status_code == 200 && response.data.success){
+                        localStorage.setItem("expiry_time",response.data.data.expires_in);
+                        var expiry = JSON.parse(localStorage.getItem("expiry_time"));
+                        $auth.removeToken();
+                        $auth.setToken(response.data.data.access_token);
+                        setInterval(function(){refresh()},60000);
+
+                    }
+                })
+            }
         }
     }]);
--- app/src/core/login/services/loginService.js
+++ app/src/core/login/services/loginService.js
@@ -1,13 +1,14 @@
 angular.module('login')
-    .service("loginService" , ['$q','$auth','$http', function ($q,$auth, $http) {
+    .factory("loginService" , ['$q','$http', function ($q,$http) {
 
         return {
-            login: login
-        }
+            login: login,
+            refresh: refresh
+        };
 
-        function login(username, password){
+        function login(data){
             var defer = $q.defer();
-                $http.get('src/core/login/assets/token.json').then(function (data) {
+            $http.get('src/core/login/assets/token.json').then(function (data) {
                     defer.resolve(data);
                 }
                 , function () {
@@ -16,4 +17,41 @@ angular.module('login')
             return defer.promise;
 
         }
+
+        function refresh(data){
+            var defer = $q.defer();
+            $http.get('src/core/login/assets/refresh.json').then(function (data) {
+                    defer.resolve(data);
+                }
+                , function () {
+                    defer.reject('could not find token.json');
+                });
+            return defer.promise;
+
+        }
+
+        // function login(data) {
+        //     return $http({
+        //         method: 'POST',
+        //         url: 'http://backend.aliens.com/api/login',
+        //         data: data
+        //     }).success(function (response) {
+        //         return response;
+        //     }).error(function () {
+        //         console.log('XHR Failed for login');
+        //     })
+        // }
+
+        // function refresh(data) {
+        //     return $http({
+        //         method: 'POST',
+        //         url: 'http://backend.aliens.com/api/refreshToken',
+        //         data: data
+        //     }).success(function (response) {
+        //         return response;
+        //     }).error(function () {
+        //         console.log('XHR Failed for login');
+        //     })
+        // }
+
     }]);
