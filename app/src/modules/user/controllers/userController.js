e0da05c8fa88cc36a3b151f01e1cb7ac44b007f0
Poonam Ranawat <p.ranawat@easternenterprise.com>
2017-06-08 14:09:06 +0530

    bug fixes

--- app/src/modules/user/controllers/userController.js
+++ app/src/modules/user/controllers/userController.js
@@ -1,5 +1,15 @@
 angular.module('user', ['notification'])
     .controller("userController" , ['$scope','userService' ,'$rootScope','CommonService','dataGetService','$timeout', function ($scope, userService, $rootScope, CommonService, dataGetService, $timeout) {
+        function deleteAllCookies() {
+            var cookies = document.cookie.split(";");
+
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i];
+                var eqPos = cookie.indexOf("=");
+                var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            }
+        }
         $scope.textsearch = "";
         $(document).keypress(
             function(event){
